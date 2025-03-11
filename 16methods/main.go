4c9e9c61c004b09c8c7e25ac1375cf2ca8697e5a
jasmeenPedalsup <jasmeen.maradeeya@pedalsup.com>
2025-03-11 16:17:16 +0530

    methods

---
+++ 16methods/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// Defining a struct named 'User' with four fields
+type User struct {
+	Name   string // Name of the user
+	Email  string // Email of the user
+	Status bool   // Active status (true/false)
+	Age    int    // Age of the user
+}
+
+func main() {
+	fmt.Println("Structs")
+
+	// Creating an instance of the User struct with values
+	jasmeen := User{"Jasmeen", "jasmeen@go.dev", true, 20}
+
+	// Print the struct directly (default formatting)
+	fmt.Println(jasmeen)
+
+	// Print struct with field names for better readability
+	fmt.Printf("jasmeen's details are: %+v \n", jasmeen)
+
+	// Accessing and printing a specific field (Name) from the struct
+	fmt.Println(jasmeen.Name)
+
+	jasmeen.GetStatus()
+	jasmeen.SetEmail()
+
+	fmt.Println(jasmeen)
+}
+
+func (u User) GetStatus() {
+	fmt.Println("Is user active: ", u.Status)
+}
+
+func (u User) SetEmail() {
+	u.Email = "bro@gmail.com"
+	fmt.Println("New email is: ", u.Email)
+}
