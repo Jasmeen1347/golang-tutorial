053e5630cbf5653101ebbeb34dcae55b53621d3a
jasmeenPedalsup <jasmeen.maradeeya@pedalsup.com>
2025-03-13 15:58:49 +0530

    Race Conditions

---
+++ 27mutextandawaitgroups/go.mod
@@ -0,0 +1,3 @@
+module mutexandawaitgroup
+
+go 1.21.4
---
+++ 27mutextandawaitgroups/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Race Conditions")
+
+	wg := &sync.WaitGroup{}
+	mut := &sync.Mutex{}
+
+	var score = []int{0}
+
+	wg.Add(3)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("One R")
+		m.Lock()
+		score = append(score, 1)
+		m.Unlock()
+		wg.Done()
+	}(wg, mut)
+
+	// wg.Add(1)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("Two R")
+		m.Lock()
+		score = append(score, 2)
+		m.Unlock()
+		wg.Done()
+	}(wg, mut)
+
+	// wg.Add(1)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("Three R")
+		m.Lock()
+		score = append(score, 3)
+		m.Unlock()
+		wg.Done()
+	}(wg, mut)
+
+	wg.Wait()
+	fmt.Println(score)
+}
+
+// go run --race . // Check for race conditions
